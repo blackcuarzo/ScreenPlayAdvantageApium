d49d7af47bc34882adc584078a9802b26ea15d6b
Daniel Cano <cd.danielcano@gmail.com>
2023-01-10 18:02:50 -0500

    Creates Stage

--- src/test/java/co/com/bancolombia/certification/advantagedemo/stepdefinitions/CreateUsersSteps.java
+++ src/test/java/co/com/bancolombia/certification/advantagedemo/stepdefinitions/CreateUsersSteps.java
@@ -1,12 +1,21 @@
 package co.com.bancolombia.certification.advantagedemo.stepdefinitions;
 
+import co.com.bancolombia.certification.advantagedemo.utils.AppiumDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.Cast;
+import net.serenitybdd.screenplay.actors.OnStage;
+
+import java.net.MalformedURLException;
 
 public class CreateUsersSteps {
     @Given("^I want to go to the page of Advantage Demo$")
-    public void iWantToGoToThePageOfAdvantageDemo() {
+    public void iWantToGoToThePageOfAdvantageDemo() throws MalformedURLException {
+        OnStage.setTheStage(Cast.ofStandardActors());
+        OnStage.theActorCalled("Daniel Cano");
+        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(AppiumDriver.app()));;
 
 
     }
