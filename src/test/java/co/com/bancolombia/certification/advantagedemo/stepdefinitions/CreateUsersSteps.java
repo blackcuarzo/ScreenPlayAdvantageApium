964bdf9ab5c8729ee0c9ebe65a0106bb6bc0b051
Daniel Cano <cd.danielcano@gmail.com>
2023-01-10 17:31:28 -0500

    Starting code

---
+++ src/test/java/co/com/bancolombia/certification/advantagedemo/stepdefinitions/CreateUsersSteps.java
@@ -0,0 +1,22 @@
+package co.com.bancolombia.certification.advantagedemo.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CreateUsersSteps {
+    @Given("^I want to go to the page of Advantage Demo$")
+    public void iWantToGoToThePageOfAdvantageDemo() {
+
+
+    }
+
+    @When("^I enter the information to create the user$")
+    public void iEnterTheInformationToCreateTheUser() {
+
+    }
+
+    @Then("^I validate the correct user was created$")
+    public void iValidateTheCorrectUserWasCreated() {
+    }
+}
